16e44a51da089a35ea33f7498b4874f42e690393
LucasDaSilva96 <dasilvajunior881@gmail.com>
2023-10-18 12:53:33 +0200

    Start settings + assets applied

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Portfolio",
+      favicon: "./src/img-icons/favIcon.png",
+    }),
+  ],
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
