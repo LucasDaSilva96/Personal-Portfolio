6783da181a9abafb528cf15fd741aa2f8dc06a43
LucasDaSilva96 <dasilvajunior881@gmail.com>
2023-10-19 12:58:17 +0200

    Imported and invoked the function in charge of rendering the hero-animation/background

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import "./style.css";
 import { renderLangList, renderHamburger } from "./nav.js";
+import { renderHeroSpline } from "./spline.js";
 
 // Language-selection
 const selectedLangBox = document.querySelector(".selected-lang");
 selectedLangBox.addEventListener("click", renderLangList);
 
 renderHamburger();
+renderHeroSpline();
