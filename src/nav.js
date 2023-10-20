508c7fafb0680e72257278e27d0990ee8854a3a8
LucasDaSilva96 <dasilvajunior881@gmail.com>
2023-10-20 13:36:43 +0200

    Finished the responsive navbar logic and design

--- src/nav.js
+++ src/nav.js
@@ -2,6 +2,7 @@ const menuContainer = document.querySelector(".lang-menu");
 const selectedLangBox = document.querySelector(".selected-lang");
 const langList = document.getElementById("lang-list");
 const langOptions = document.querySelectorAll(".lang-opt");
+const sectionsNavContainer = document.querySelector(".sections-container");
 
 export function renderLangList() {
   langList.classList.toggle("hidden");
@@ -23,5 +24,6 @@ export function renderHamburger() {
   const hamburger = document.querySelector(".hamburger");
   hamburger.addEventListener("click", function () {
     hamburger.classList.toggle("is-active");
+    sectionsNavContainer.classList.toggle("show-menu");
   });
 }
