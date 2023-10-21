03a5499be412cf1a53f741ae0fc6716ab6de6fd3
LucasDaSilva96 <dasilvajunior881@gmail.com>
2023-10-21 17:00:32 +0200

    Finished making the portfolio section and carousel responsive

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
 import "./style.css";
 import { renderLangList, renderHamburger, hideNav } from "./nav.js";
 import { renderHeroSpline } from "./spline.js";
-import {
-  dragStart,
-  dragStop,
-  dragging,
-  scrollLeft,
-  scrollRight,
-} from "./projects.js";
-
+import { nextSlide, prevSlide } from "./projects.js";
 // Language-selection
 const selectedLangBox = document.querySelector(".selected-lang");
 selectedLangBox.addEventListener("click", renderLangList);
@@ -16,15 +9,5 @@ selectedLangBox.addEventListener("click", renderLangList);
 renderHamburger();
 // renderHeroSpline();
 hideNav();
-
-const carousel = document.querySelector(".project-box");
-
-carousel.addEventListener("mousedown", dragStart);
-carousel.addEventListener("touchstart", dragStart);
-
-carousel.addEventListener("mousemove", dragging);
-carousel.addEventListener("touchmove", dragging);
-carousel.addEventListener("mouseup", dragStop);
-carousel.addEventListener("touchend", dragStop);
-scrollRight();
-scrollLeft();
+nextSlide();
+prevSlide();
--- src/projects.js
+++ src/projects.js
@@ -1,57 +1,35 @@
-const carousel = document.querySelector(".project-box");
-const firstImage = carousel.querySelectorAll("img")[0];
-const leftArrow = document.querySelector(".left-icon");
-const rightArrow = document.querySelector(".right-icon");
+const projectBox = document.querySelector(".project-box");
+const prev = document.querySelector(".left");
+const next = document.querySelector(".right");
+const scrollContainer = document.querySelector(".projects-container");
 
-let firstImageWidth = firstImage.clientWidth;
-const AmountOfImgs = carousel.querySelectorAll("img").length;
-const allImages = carousel.querySelectorAll("img");
-let COUNTER = 0;
+const image_width = 410;
+let COUNTER = 410;
+const MAX = scrollContainer.scrollWidth;
 
-let isDragStart = false,
-  prevPageX,
-  prevScrollLeft;
-
-export function dragStart(e) {
-  isDragStart = true;
-  prevPageX = e.pageX || e.touches[0].pageX;
-  prevScrollLeft = carousel.scrollLeft;
-}
-
-export function dragStop() {
-  isDragStart = false;
-}
-
-export function dragging(e) {
-  if (!isDragStart) return;
-  e.preventDefault();
-  let positionDiff = (e.pageX || e.touches[0].pageX) - prevPageX;
-  carousel.scrollLeft = prevScrollLeft - positionDiff;
-}
-
-export function scrollRight() {
-  rightArrow.addEventListener("click", function () {
-    carousel.scrollLeft += firstImageWidth;
-    COUNTER++;
-    showHideScroll(COUNTER);
+export function nextSlide() {
+  next.addEventListener("click", () => {
+    if (COUNTER + image_width < MAX - 205) {
+      COUNTER += image_width;
+    } else {
+      next.classList.add("hidden");
+      return;
+    }
+    projectBox.style.transform = `translateX(-${COUNTER}px)`;
+    prev.classList.remove("hidden");
+    console.log(COUNTER);
   });
 }
 
-export function scrollLeft() {
-  leftArrow.addEventListener("click", function () {
-    carousel.scrollLeft += -firstImageWidth;
-    COUNTER--;
-    showHideScroll(COUNTER);
+export function prevSlide() {
+  prev.addEventListener("click", () => {
+    if (COUNTER > image_width - image_width) {
+      COUNTER -= image_width;
+    } else {
+      prev.classList.add("hidden");
+      return;
+    }
+    projectBox.style.transform = `translateX(-${COUNTER}px)`;
+    next.classList.remove("hidden");
   });
-}
-
-function showHideScroll(COUNTER) {
-  if (COUNTER < 1) {
-    leftArrow.classList.add("hidden");
-  } else if (COUNTER === AmountOfImgs - 1) {
-    rightArrow.classList.add("hidden");
-  } else {
-    leftArrow.classList.remove("hidden");
-    rightArrow.classList.remove("hidden");
-  }
 }
