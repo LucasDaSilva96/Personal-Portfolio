f3675d26b4d1f1c5118a88df4561dbf097041c07
LucasDaSilva96 <dasilvajunior881@gmail.com>
2023-10-21 21:29:26 +0200

    Finished the contact section, and completed the form.

---
+++ src/form.js
@@ -0,0 +1,5 @@
+const form = document.querySelector("form");
+
+export function formAction() {
+  return (form.action = "https://formsubmit.co/dasilvajunior881@gmail.com");
+}
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import "./style.css";
 import { renderLangList, renderHamburger, hideNav } from "./nav.js";
 import { renderHeroSpline } from "./spline.js";
 import { nextSlide, prevSlide } from "./projects.js";
+import { formAction } from "./form";
 // Language-selection
 const selectedLangBox = document.querySelector(".selected-lang");
 selectedLangBox.addEventListener("click", renderLangList);
 
 renderHamburger();
-// renderHeroSpline();
+renderHeroSpline();
 hideNav();
 nextSlide();
 prevSlide();
+formAction();
