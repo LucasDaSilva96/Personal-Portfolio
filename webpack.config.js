2f266aca5f0b911d72b2cbdb1505bb6604005abe
LucasDaSilva96 <dasilvajunior881@gmail.com>
2023-10-22 22:33:59 +0200

    Hade to install html plugin in order for all the images and video to load properly

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,16 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
 
 module.exports = {
-  mode: "production",
+  mode: "development",
   entry: "./src/index.js",
   devtool: "inline-source-map",
   plugins: [
+    new FaviconsWebpackPlugin({
+      logo: "./src/img/favIcon.png",
+    }),
     new HtmlWebpackPlugin({
       title: "Portfolio",
       template: "template.html",
-    }),
-    new FaviconsWebpackPlugin({
-      logo: "./src/img-icons/favIcon.png",
     }),
   ],
   output: {
@@ -23,8 +23,20 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
         test: /\.(?:js|mjs|cjs)$/,
-        exclude: /node_modules/,
+        exclude: {
+          and: [/node_modules/], // Exclude libraries in node_modules ...
+          not: [
+            // Except for a few of them that needs to be transpiled because they use modern syntax
+            /unfetch/,
+            /d3-array|d3-scale/,
+            /@hapi[\\/]joi-date/,
+          ],
+        },
         use: {
           loader: "babel-loader",
           options: {
